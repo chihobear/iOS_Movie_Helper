2a9fa10ecb6935aa2c8028f143b100677c61e995
Chenghao Xiong <chenghaoxiong@chenghaodemacbook-pro.local>
2019-02-10 15:03:30 -0700

    Updated

---
+++ WeekOne/MovieGridCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCollectionViewCell.swift
+//  WeekOne
+//
+//  Created by Chenghao Xiong on 2/10/19.
+//  Copyright © 2019 Chenghao Xiong. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
